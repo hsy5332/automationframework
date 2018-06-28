985896fb4540ef18aff0bc0692ce18cd4da2cadd
steel <allenyao224@qq.com>
2018-06-28 20:24:18 +0800

    获取adb

--- automationframework/automationserver/views.py
+++ automationframework/automationserver/views.py
@@ -11,8 +11,13 @@ def index(request):
 
 # 获取本地adb的设备信息
 def adb_devices(request):
-    take_devices_info = os.system("adb devices")
-    return HttpResponse(take_devices_info)
+    take_devices_list = []
+    take_devices_info = os.popen("adb devices")
+    for i in take_devices_info:
+        if 'device' in i and '5555' in i:
+            take_devices_list.append(i.split('\t')[0])
+
+    return HttpResponse(take_devices_list[0])
 
 def testone(request):
     return adb_devices("GET")
