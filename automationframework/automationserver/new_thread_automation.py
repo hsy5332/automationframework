ed9b62304cfe3fa5fce2a373c4815140a9b8d9f7
鈊诺 <allenyao224@qq.com>
2018-07-01 22:04:18 +0800

    启动Appium

--- automationframework/automationserver/new_thread_automation.py
+++ automationframework/automationserver/new_thread_automation.py
@@ -1,22 +1,25 @@
 # coding : utf-8
 import threading
+import run_app_automation
 
 
 class NewThreadAutomation(threading.Thread):
-    def __init__(self):
+    def __init__(self, device_id):
         threading.Thread.__init__(self)
+        self.device_id = device_id
 
     def run(self):
-        pass
+        print(self.device_id)
 
 
 # 创建线程
+count = 0;
 threads = []
-threadcount = 0
-while threadcount < 3:
-    thread = NewThreadAutomation()
+thread_count, device_list, appium_port, appium_bootstrap_port, pid_list = run_app_automation.RunAppAutomation().launch_appium()  # 获取设备数、设备名称、appium、appium bootstrap端口号
+while count < thread_count:
+    thread = NewThreadAutomation(device_list[count])
     threads.append(thread)
-    threadcount = threadcount + 1;
+    count += 1;
 for i in threads:
     i.start()
 for y in threads:
---
+++ automationframework/automationserver/run_app_automation.py
@@ -0,0 +1,49 @@
+# coding : utf-8
+
+import os
+import random
+
+
+class RunAppAutomation:
+
+    # 获取本地设备信息
+    def get_device(self):
+        devices_list = []  # 存放设备列表
+        for device_id in os.popen("adb devices"):
+            if 'device' in device_id and 'attached' not in device_id:
+                devices_list.append(device_id.split('\t')[0])
+        device_count = len(devices_list)  # 获取的设备数
+        appium_port, appium_bootstrap_port = RunAppAutomation.create_appium_port(self, device_count)
+        return device_count, devices_list, appium_port, appium_bootstrap_port
+
+    def create_appium_port(self, device_count):
+        count = 0;
+        appium_port = []  # 存放生成appium的端口号
+        appium_bootstrap_port = []  # 存放生成appium bootstrap的端口号
+        while count < device_count:
+            random_port_number = random.randrange(4000, 10000, 2)
+            random_bootstrap_number = random.randrange(4001, 9999, 2)
+            if random_port_number not in appium_port and random_bootstrap_number not in appium_bootstrap_port:
+                appium_port.append(random_port_number)  # 获取1000 到 100000之间的偶数
+                appium_bootstrap_port.append(random_bootstrap_number)  # 获取1001 到 9999之间的奇数
+                count += 1;
+        return appium_port, appium_bootstrap_port
+
+    def launch_appium(self):
+        device_count, devices_list, appium_port, appium_bootstrap_port = RunAppAutomation().get_device()
+        appium_port_count = 0;
+        pid_count = 0;
+        pid_list = []
+        while appium_port_count < device_count:
+            os.popen("appium -p %s -bp %s" % (appium_port[appium_port_count], appium_bootstrap_port[appium_port_count]))
+            appium_port_count += 1;
+
+        while pid_count < device_count:
+            for pid in os.popen("netstat -ano | findstr %s" % appium_port[pid_count]):
+                pid_list.append(pid.replace("\t", '').replace(" ", '').strip().split('LISTENING')[1])
+            pid_count += 1;
+        return device_count, devices_list, appium_port, appium_bootstrap_port, pid_list
+
+
+if __name__ == "__main__":
+    RunAppAutomation().launch_appium()
