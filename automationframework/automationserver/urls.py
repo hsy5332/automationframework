71ac7bdabc1d94c07dc4b36d76b3cd9cd76d8621
鈊诺 <allenyao224@qq.com>
2018-07-06 00:43:12 +0800

    增加静态文件访问

--- automationframework/automationserver/urls.py
+++ automationframework/automationserver/urls.py
@@ -2,12 +2,13 @@
 from django.conf.urls import url
 from automationserver import views
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^index', views.index),  # 示例
-    url(r'^devicelist', views.get_devices),  # 获取设备
-    url(r'^connectdevices', views.connect_devices),  # 连接设备
-    url(r'^disconnectdevice', views.disconnection_devices),  # 断开设备
-    url(r'^runcase',views.run_automation_file) # 执行自动化用例接口
-    # url(r'^testone', views.testone)
-]
+                  url(r'^index', views.index),  # 示例
+                  url(r'^devicelist', views.get_devices),  # 获取设备
+                  url(r'^connectdevices', views.connect_devices),  # 连接设备
+                  url(r'^disconnectdevice', views.disconnection_devices),  # 断开设备
+                  url(r'^runcase', views.run_automation_file)  # 执行自动化用例接口
+                  # url(r'^testone', views.testone)
+              ] + static(settings.STATIC_URL, docment_root=settings.STATIC_ROOT)
