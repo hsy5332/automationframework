1867b545f91f144562f70ff42ea5d2d0e328c09a
steel_mac_book <allenyao224@qq.com>
2018-08-16 23:20:41 +0800

    完成celery 消息队列

--- automationframework/automationserver/tasks.py
+++ automationframework/automationserver/tasks.py
@@ -1,19 +1,16 @@
 # conding:utf-8
 import time
 from automationframework import celery_app
-from . import data_read, new_thread_automation, run_web_automation, run_interface_automation
+from . import new_thread_automation, run_interface_automation
 
 
 @celery_app.task
 def run_automation_task(file_name, run_case_type):
-    # new_thread_automation.run_automation_procedure(file_name, run_case_type)
-    print(file_name, run_case_type)
+    if run_case_type == 'app':
+        new_thread_automation.run_automation_procedure(file_name, run_case_type)  # app 自动化
+    elif run_case_type == 'interface':
+        run_interface_automation.RunInterfaceAutomation(file_name, run_case_type).run_interface_case()  # 接口自动化
+    else:
+        pass
     time.sleep(30)
     return file_name, run_case_type
-
-@celery_app.task
-def add(x, y):
-    print(x, y)
-    time.sleep(30)
-    return x + y
-
--- automationframework/automationserver/urls.py
+++ automationframework/automationserver/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
                   url(r'^devicelist', views.get_devices),  # 获取设备
                   url(r'^connectdevices', views.connect_devices),  # 连接设备
                   url(r'^disconnectdevice', views.disconnection_devices),  # 断开设备
-                  url(r'^runcase', views.run_automation_file), # 执行自动化用例接口
-                  url(r'^addone', views.addone)
+                  url(r'^runcase', views.run_automation_file),  # 执行自动化用例接口
+                  # url(r'^testone', views.testone)
               ] + static(settings.STATIC_URL, docment_root=settings.STATIC_ROOT)
