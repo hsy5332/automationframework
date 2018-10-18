87dd7ba28c4a6aadba88519a35cf27652d7bd722
steel_mac_book <allenyao224@qq.com>
2018-10-18 16:43:08 +0800

    celery 增加web自动化任务。

--- automationframework/automationserver/tasks.py
+++ automationframework/automationserver/tasks.py
@@ -1,7 +1,7 @@
 # conding:utf-8
 import time
 from automationframework import celery_app
-from . import new_thread_automation, run_interface_automation
+from . import new_thread_automation, run_interface_automation, run_web_automation
 
 
 @celery_app.task
@@ -13,4 +13,10 @@ def run_automation_app_task(file_name, run_case_type, devices_id):
 @celery_app.task
 def run_automation_interface_task(file_name, run_case_type):
     run_interface_automation.RunInterfaceAutomation(file_name, run_case_type).run_interface_case()  # 接口自动化
+    return file_name, run_case_type
+
+
+@celery_app.task
+def run_automation_web_task(file_name, run_case_type):
+    run_web_automation.RunWebAutomation().run_web_case(file_name, run_case_type)  # web自动化
     return file_name, run_case_type
